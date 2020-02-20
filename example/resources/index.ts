e15ade4974aae7f5b61e6331f8c083f30e210afb
Rushi Patel <rpatel@nextfaze.com>
2020-02-20 18:19:22 +1030

    init example app

--- example/resources/index.ts
+++ example/resources/index.ts
@@ -1,1 +1,63 @@
-const x = 'working';
+import { App } from '@aws-cdk/core';
+import {
+  DoostrapperDelivery,
+  NOTIFICATIONS_TARGET,
+  NOTIFICATIONS_TYPE,
+} from 'doostrapper';
+const buildSpec = {
+  phases: {
+    install: {
+      'runtime-versions': {
+        docker: 18,
+      },
+    },
+    build: {
+      commands: ['echo Hello from $currEnv', 'docker version'],
+    },
+  },
+};
+
+const app = new App();
+new DoostrapperDelivery(app, 'Doostrapper', {
+  pipelineConfig: {
+    artifactsSourceKey: 'artifacts/example.zip',
+    environments: [
+      {
+        name: 'dev',
+        adminPermissions: true,
+        runtimeVariables: {
+          currEnv: 'Development',
+        },
+        buildSpec,
+      },
+      {
+        name: 'uat',
+        adminPermissions: true,
+        runtimeVariables: {
+          currEnv: 'User Acceptance Testing',
+        },
+        buildSpec,
+      },
+      {
+        name: 'prod',
+        adminPermissions: true,
+        approvalRequired: true,
+        runtimeVariables: {
+          currEnv: 'production',
+        },
+        buildSpec,
+      },
+    ],
+  },
+  notificationsConfig: {
+    notificationsTargetConfig: {
+      emailAddress: 'rpatel+test@nextfaze.com',
+      emailSubject: 'Deployment Notifications',
+      targetType: NOTIFICATIONS_TARGET.EMAIL,
+    },
+    notificationsType: NOTIFICATIONS_TYPE.PIPELINE_EXECUTION,
+  },
+  artifactsBucketConfig: {
+    bucketName: 'example-doostrapper-artifacts-bucket',
+  },
+});
