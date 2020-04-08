ffcf9e9a5b8102f2866d6d98de198b1add8aa4de
whimzyLive <rp970.patel@gmail.com>
2020-04-08 14:18:59 +0930

    chore: replace static version with placeholder

--- tools/bazel_stamp_vars.js
+++ tools/bazel_stamp_vars.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 const execSync = require('child_process').execSync;
+const argsWithoutNode = process.argv.slice(1);
 process.on('uncaughtException', function(err) {
-  console.error('Failed to execute:,', process.argv.join(' '));
-  console.error(err);
+  console.error('Failed to execute:', argsWithoutNode.join(' '));
 });
 
 const BUILD_SCM_HASH = _exec(`git rev-parse HEAD`);
-console.log(`BUILD_SCM_HASH: ${BUILD_SCM_HASH}`);
+console.log(`BUILD_SCM_HASH ${BUILD_SCM_HASH}`);
 
 const currentTag = _exec(`git tag`);
 if (!currentTag) {
@@ -19,13 +19,14 @@ if (!currentTag) {
 }
 
 const BUILD_SCM_VERSION_RAW = _exec(
-  `git describe --match [0-9].[0-9].[0-9]* --abbrev=7 --tags HEAD`
+  `git describe --match v[0-9].[0-9].[0-9]* --abbrev=7 --tags HEAD`
 );
 
 const BUILD_SCM_VERSION = BUILD_SCM_VERSION_RAW.replace(
   /-([0-9]+)-g/,
   '+$1.sha-'
 );
+
 console.log(`BUILD_SCM_VERSION ${BUILD_SCM_VERSION}`);
 
 function _exec(command) {
